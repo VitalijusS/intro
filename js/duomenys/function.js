03b55c71794809ab9722b329a98fafb36c9c5d5e
VitalijusS <v.stifanovicius@gmail.com>
2024-05-03 11:39:15 +0300

    alphabet

--- js/duomenys/function.js
+++ js/duomenys/function.js
@@ -70,4 +70,28 @@ add()
 add()
 add()
 add()
-console.log(number)
+
+
+console.clear()
+function isPangram(string){
+    const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+    for (let i = 0; i < alphabet.length; i++){
+        let isIn = string.includes(alphabet[i]);
+        if (isIn == false){
+            return false           
+        }
+    }
+    return true
+}
+
+
+console.log(isPangram("a"))
+console.log()
+console.log(isPangram("1"))
+console.log()
+console.log(isPangram("qwertyuioplkjhgfdsazxcvbnm"))
+console.log()
+console.log(isPangram("The quick brown fox jumps over the lazy dog."))
+console.log()
+console.log(isPangram("asd321"))
+
