2e70aed3ba54bac24ab8f7a68b1593f6731ee642
VitalijusS <v.stifanovicius@gmail.com>
2024-05-03 14:08:50 +0300

    task

--- js/duomenys/function.js
+++ js/duomenys/function.js
@@ -113,6 +113,79 @@ console.log(squareDigits(123456));
 console.log(squareDigits(123));
 console.log(squareDigits(3212));
 
+console.clear();
+
+function name(name){
+    return console.log(`Labas, ${name}`)
+}
+
+name("Elena")
 
 
+function simbol(text){
+    return console.log(text.length)
+}
 
+simbol("asdASDFsd45");
+
+console.clear();
+
+/* Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+
+Use the BMI example from Challenge #1, and the code you already wrote, and
+improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement 😉 */
+
+const mark1W = 78;
+const mark1H = 1.69;
+const john1W = 92;
+const john1H = 1.95;
+
+const mark2W = 95;
+const mark2H = 1.88;
+const john2W = 85;
+const john2H = 1.76;
+
+const mark1BMI = mark1W / (mark1H ** 2);
+const john1BMI = john1W / (john1H ** 2);
+const mark2BMI = mark2W / (mark2H ** 2);
+const john2BMI = john2W / (john2H ** 2);
+
+const markHigherBMI = mark1BMI > john1BMI;
+const markHigherBMI2 = mark2BMI > john2BMI;
+
+console.log(markHigherBMI)
+console.log(mark1BMI, john1BMI)
+console.log(markHigherBMI2)
+console.log(mark2BMI, john2BMI)
+
+if (mark1BMI > john1BMI){
+    console.log(`Mark's BMI ${mark1BMI.toFixed(1)} is higher than John's ${john1BMI.toFixed(1)}!`)
+}else{
+    console.log(`John's ${john1BMI.toFixed(1)} is higher than Mark's BMI ${mark1BMI.toFixed(1)}!`)
+}
+
+if (mark2BMI > john2BMI){
+    console.log(`Mark's BMI ${mark2BMI.toFixed(1)} is higher than John's ${john2BMI.toFixed(1)}!`)
+}else{
+    console.log(`John's ${john2BMI.toFixed(1)} is higher than Mark's BMI ${mark2BMI.toFixed(1)}!`)
+}
