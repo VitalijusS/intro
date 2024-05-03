ed2aafbd68bb374fbe50846f5d65abfbc111a4dc
VitalijusS <v.stifanovicius@gmail.com>
2024-05-03 11:41:23 +0300

    alphabet

--- js/duomenys/function.js
+++ js/duomenys/function.js
@@ -76,8 +76,7 @@ console.clear()
 function isPangram(string){
     const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
     for (let i = 0; i < alphabet.length; i++){
-        let isIn = string.includes(alphabet[i]);
-        if (isIn == false){
+        if (string.includes(alphabet[i]) == false){
             return false           
         }
     }
